95b2cd72124955b8ef97ec5e17838806034af7e6
emmafattori <emmalfattori@gmail.com>
2024-08-26 21:26:49 -0400

    Updating note page to list notes, adding pocketbase package,

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,43 +1,63 @@
-import Link from "next/link";
-import styles from './Notes.module.css'
+import PocketBase from 'pocketbase';
+import Link from 'next/link';
+// import styles from './Notes.module.css';
+// import CreateNote from './Create';
 
-async function getNotes () {
-    const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records');
+// export const dynamic = 'auto',
+//   dynamicParams = true,
+//   revalidate = 0,
+//   fetchCache = 'auto',
+//   runtime = 'nodejs',
+//   preferredRegion = 'auto'
 
-    // The components are built on the server so you can do data fetching in them as well.
-    const data = await res.json();
-    console.log(data)
-    return data?.items as any[];
-    
+const pb = new PocketBase('http://127.0.0.1:8090');
+
+
+async function getNotes() {
+    pb.autoCancellation(false)
+const db = new PocketBase('http://127.0.0.1:8090');
+
+//   const result = await db.rec
+  const res = await fetch('http://127.0.0.1:8090/api/collections/notes/records?page=1&perPage=30', { cache: 'no-store' });
+  const data = await res.json();
+  return data?.items as any[];
 }
-
 
 export default async function NotesPage() {
+    console.log('it worked')
+  const notes = await getNotes();
+  console.log(notes, 'notes')
 
-    const notes = await getNotes();
-    console.log(notes)
-    return (
-        <div>
-            <h1>Notes</h1>
-            <div className={styles.grid}>
-                {notes?.map((note) => {
-                    return <Note key={note.id} note={note} />;
-                })}
-            </div>
-        </div>
-    )
+  return(
+    <div>
+      <h1>Notes</h1>
+      <div>
+        {notes?.map((note) => {
+            console.log(note)
+          return (
+            <>
+            <h2>{note.name}</h2>
+            <Note key={note.id} note={note} />
+            </>
+          );
+        })}
+      </div>
+
+      {/* <CreateNote /> */}
+    </div>
+  );
 }
 
-const Note = ({note}: any) => {
-const {id, title, content, created} = note || {}
+function Note({ note }: any) {
+  const { id, title, content, created } = note || {};
 
-return (
+  return (
     <Link href={`/notes/${id}`}>
-    <div>
+      <div>
         <h2>{title}</h2>
         <h5>{content}</h5>
         <p>{created}</p>
-    </div>
+      </div>
     </Link>
-)
+  );
 }
